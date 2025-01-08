fb2cdcb82c8512821a482c203e5e3ca7ed8237e0
yasser-eddouche <eddouchey@gmail.com>
2025-01-08 11:56:59 +0100

    Add utility functions for MongoDB

--- src/services/mongoService.js
+++ src/services/mongoService.js
@@ -1,14 +1,35 @@
 // Question: Pourquoi créer des services séparés ?
-// Réponse: 
+// Réponse: Créer des services séparés permet de structurer le code de manière modulaire, de réutiliser les fonctions utilitaires à travers l'application, et de faciliter la maintenance et les tests.
 
-const { ObjectId } = require('mongodb');
+const { ObjectId } = require("mongodb");
 
 // Fonctions utilitaires pour MongoDB
 async function findOneById(collection, id) {
-  // TODO: Implémenter une fonction générique de recherche par ID
+  try {
+    if (!ObjectId.isValid(id)) {
+      throw new Error("Invalid ID format");
+    }
+    const objectId = ObjectId.createFromHexString(id);
+    const result = await collection.findOne({ _id: objectId });
+    return result;
+  } catch (error) {
+    console.error("Error finding document by ID:", error);
+    throw error;
+  }
+}
+
+async function insertOne(collection, data) {
+  try {
+    const result = await collection.insertOne(data);
+    return result;
+  } catch (error) {
+    console.error("Error inserting document:", error);
+    throw error;
+  }
 }
 
 // Export des services
 module.exports = {
-  // TODO: Exporter les fonctions utilitaires
+  findOneById,
+  insertOne,
 };
